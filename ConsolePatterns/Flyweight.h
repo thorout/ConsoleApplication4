6582f9285d39bd2f56b4f1ce1aab60a0810a83f6
thorout <thorout@gmail.com>
2018-07-08 00:36:58 +0300

    add Flyweight pattern complete

--- ConsolePatterns/Flyweight.h
+++ ConsolePatterns/Flyweight.h
@@ -1,6 +1,35 @@
 #pragma once
 #include <iostream>
 
+class MassObject
+{
+public:
+	MassObject(int number) : number_(number) { }
+	int GetData() { return number_; }
+private:
+	int number_;
+};
+
+
+class IFlyweight
+{
+public:
+	virtual ~IFlyweight() { }
+	virtual void draw(MassObject& massObject) = 0;
+};
+
+
+class ConcreteFlyweight : public IFlyweight
+{
+public:
+	ConcreteFlyweight(char c) : bigData_(c) { }
+	virtual ~ConcreteFlyweight() { }
+	void draw(MassObject& massObject) override { std::cout << bigData_ << massObject.GetData(); }
+private:
+	char bigData_;
+};
+
+
 class Flyweight
 {
 public:
