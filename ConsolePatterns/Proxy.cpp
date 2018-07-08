5950150c3d72bfac4cb2c33ddd16b83c19ebf322
thorout <thorout@gmail.com>
2018-07-08 18:38:11 +0300

    add proxy pattern layout

---
+++ ConsolePatterns/Proxy.cpp
@@ -0,0 +1,19 @@
+#include "Proxy.h"
+
+
+
+Proxy::Proxy()
+{
+	std::cout << "Pattern \"Proxy\"\n";
+}
+
+
+Proxy::~Proxy()
+{
+}
+
+
+void Proxy::run()
+{
+	std::cout << ">>> Whole Pattern was terminated...\n";
+}
