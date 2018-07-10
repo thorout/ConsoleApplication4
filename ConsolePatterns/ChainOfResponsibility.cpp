b88510330ca470bd4458a549537a64a69dca04d8
thorout <thorout@gmail.com>
2018-07-10 17:54:10 +0300

    add chain of responsibility pattern complete

--- ConsolePatterns/ChainOfResponsibility.cpp
+++ ConsolePatterns/ChainOfResponsibility.cpp
@@ -15,5 +15,12 @@ ChainOfResponsibility::~ChainOfResponsibility()
 
 void ChainOfResponsibility::run()
 {
+	ConceteHandlerA chA01(nullptr);
+	ConceteHandlerA chA02(&chA01);
+	ConceteHandlerA chA03(&chA02);
+	ConceteHandlerA chA04(&chA03);
+
+	chA04.HandleRequest();
+
 	std::cout << ">>> Whole Pattern was terminated...\n";
 }
--- ConsolePatterns/ChainOfResponsibility.h
+++ ConsolePatterns/ChainOfResponsibility.h
@@ -1,5 +1,39 @@
 #pragma once
 #include <iostream>
+
+
+class Handler
+{
+public:
+	Handler(Handler* h) : handler_(h) {	}
+	virtual ~Handler() { }
+	virtual void HandleRequest() 
+	{
+		if (handler_)
+		{
+			std::cout << "Handle to another handller" << std::endl;
+			handler_->HandleRequest();
+		}
+		else
+		{
+			std::cout << "There is no handlers..." << std::endl;
+		}
+	}
+private:
+	Handler* handler_;
+
+};
+
+class ConceteHandlerA : public Handler
+{
+public:
+	ConceteHandlerA(Handler* h) : Handler(h)
+	{
+	}
+};
+
+
+
 class ChainOfResponsibility
 {
 public:
