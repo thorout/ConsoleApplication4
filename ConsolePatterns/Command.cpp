2b023a40b19a993f766b201f907cb6ffe49ebf3a
thorout <thorout@gmail.com>
2018-07-12 09:28:06 +0300

    extend command pattern by history feature

--- ConsolePatterns/Command.cpp
+++ ConsolePatterns/Command.cpp
@@ -2,6 +2,7 @@
 
 
 
+
 Command::Command()
 {
 	std::cout << "Pattern \"Command\"\n";
@@ -16,9 +17,21 @@ Command::~Command()
 void Command::run()
 {
 	Document doc;
+	doc.SetText("Initial TEXT.");
 	ICommand* command = new ConcreteCommandA(&doc);
 	Menu menu;
 	menu.SetCommand(command);
+
+	History<ICommand> history;
+
+	std::cout << " >>> " << doc.GetText() << std::endl;
 	menu.Click();
+	history.Push(command);
+	std::cout << " >>> " << doc.GetText() << std::endl;
+	//command->unexecute();
+	history.Pop()->unexecute();
+	std::cout << " >>> " << doc.GetText() << std::endl;
+	
+
 	std::cout << ">>> Whole Pattern was terminated...\n";
 }
--- ConsolePatterns/Command.h
+++ ConsolePatterns/Command.h
@@ -1,5 +1,29 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <stack>
+#include <memory>
+
+template <class T>
+class History
+{
+public:
+	void Push(T* command)
+	{
+		history_.push(command->Clone());
+	}
+
+	T* Pop()
+	{
+		T* copy = history_.top();
+		history_.pop();
+		return copy;
+	}
+
+private:
+	std::stack<T*> history_;
+};
+
 
 class Document
 {
@@ -7,7 +31,19 @@ public:
 	void SomeAction()
 	{
 		std::cout << "Document : Doing some action!" << std::endl;
+		text_ = "Some text after command implementation.";
 	}
+	void SetText(std::string t)
+	{
+		text_ = t;
+	}
+	std::string GetText()
+	{
+		return text_;
+	}
+
+private:
+	std::string text_ = "";
 };
 
 
@@ -16,7 +52,11 @@ class ICommand
 public:
 	virtual ~ICommand() { }
 	virtual void execute() = 0;
+	virtual void unexecute() = 0;
+	virtual ICommand* Clone() = 0;
 };
+
+
 
 class ConcreteCommandA : public ICommand
 {
@@ -26,10 +66,25 @@ public:
 	void execute() override
 	{
 		std::cout << "Command is Executed..." << std::endl;
+		Backup();
 		document_->SomeAction();
+
 	}
+	void unexecute() override
+	{
+		std::cout << "Command is Unexecuted... Restore document state.." << std::endl;
+		document_->SetText(backup_);
+	}
+
+	ICommand* Clone() override
+	{
+		return new ConcreteCommandA(*this);
+	}
+
 private:
+	void Backup() { backup_ = document_->GetText(); }
 	Document* document_;
+	std::string backup_;
 };
 
 class Menu
