2621f6db8c667b43f1dcb4602a0fd40397bc3384
thorout <thorout@gmail.com>
2018-07-15 21:07:15 +0300

    add mediator pattern complete

--- ConsolePatterns/Mediator.cpp
+++ ConsolePatterns/Mediator.cpp
@@ -13,7 +13,25 @@ Mediator::~Mediator()
 }
 
 
+void ConcreteMediator::WidgetChanged(IColleague* widget)
+{
+	if (widget->getStatus())
+	{
+		dynamic_cast<ConcreteColleagueB*>(widget)->SetVisible();
+	}
+}
+
+
+
 void Mediator::run()
 {
+	IMediator* mediator = new ConcreteMediator();
+	IColleague* widgetA = new ConcreteColleagueA(mediator);
+	IColleague* widgetB = new ConcreteColleagueB(mediator);
+	dynamic_cast<ConcreteColleagueA*>(widgetA)->CheckIn();
+
+	delete widgetB;
+	delete widgetA;
+	delete mediator;
 	std::cout << ">>> Whole Pattern was terminated...\n";
 }
--- ConsolePatterns/Mediator.h
+++ ConsolePatterns/Mediator.h
@@ -1,5 +1,94 @@
 #pragma once
 #include <iostream>
+#include <memory>
+
+class IColleague;
+
+class IMediator
+{
+public:
+	IMediator()
+	{
+	}
+
+	virtual ~IMediator()
+	{
+	}
+
+	virtual void WidgetChanged(IColleague* widget) = 0;
+
+private:
+
+};
+
+
+class ConcreteMediator : public IMediator
+{
+public:
+	ConcreteMediator()
+	{
+	}
+
+	~ConcreteMediator()
+	{
+	}
+
+	void WidgetChanged(IColleague* widget) override;
+
+private:
+
+};
+
+
+class IColleague
+{
+public:
+	explicit IColleague(IMediator* director) : director_(director)	{ }
+
+	virtual ~IColleague() {	}
+
+	virtual void Changed()
+	{
+		director_->WidgetChanged(this);
+	}
+
+	bool getStatus()
+	{
+		return status_;
+	}
+
+private:
+	IMediator* director_;
+protected:
+	bool status_ = false;
+};
+
+class ConcreteColleagueA : public IColleague
+{
+public:
+	ConcreteColleagueA(IMediator* director) : IColleague(director) { }
+	void CheckIn()
+	{
+		std::cout << "WidgetA is checked in." << std::endl;
+		status_ = true;
+		Changed();
+	}
+private:
+
+};
+
+class ConcreteColleagueB : public IColleague
+{
+public:
+	ConcreteColleagueB(IMediator* director) : IColleague(director) { }
+	void SetVisible() 
+	{
+		std::cout << "WidgetB is visible." << std::endl;
+	}
+};
+
+
+
 class Mediator
 {
 public:
