b1ac9dcb06ccad8995a0576bad15f338c0a3668f
thorout <thorout@gmail.com>
2018-07-20 18:26:15 +0300

    add visitor pattern complete

--- ConsolePatterns/Visitor.cpp
+++ ConsolePatterns/Visitor.cpp
@@ -1,6 +1,7 @@
 #include "Visitor.h"
-
-
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 Visitor::Visitor()
 {
@@ -13,7 +14,68 @@ Visitor::~Visitor()
 }
 
 
+
+
+inline void ConcreteVisitor01::VisitConcreteElementA(ConcreteElementA * elementA)
+{
+	elementsA_counter_++;
+	std::cout << "Concrete Visitor 01 for Element A is run. Element data is equal " << elementA->OperationA() << ". Counter of processed elements is " << elementsA_counter_ << std::endl;
+}
+
+inline void ConcreteVisitor01::VisitConcreteElementB(ConcreteElementB * elementB)
+{
+	elementsB_counter_++;
+	std::cout << "Concrete Visitor 01 for Element B is run. Element data is equal " << elementB->OperationB() << ". Counter of processed elements is " << elementsB_counter_ << std::endl;
+}
+
+inline void ConcreteVisitor02::VisitConcreteElementA(ConcreteElementA * elementA)
+{
+	elementsA_counter_++;
+	std::cout << "Concrete Visitor 02 for Element A is run. Element data is equal " << elementA->OperationA() << ". Counter of processed elements is " << elementsA_counter_ << std::endl;
+}
+
+inline void ConcreteVisitor02::VisitConcreteElementB(ConcreteElementB * elementB)
+{
+	elementsB_counter_++;
+	std::cout << "Concrete Visitor 02 for Element B is run. Element data is equal " << elementB->OperationB() << ". Counter of processed elements is " << elementsB_counter_ << std::endl;
+}
+
+
+
+
 void Visitor::run()
 {
+	std::vector<Element*> vector_of_elements;
+
+	srand(time(NULL));
+	for (size_t i = 0; i < 10; i++)
+	{
+		int r = rand() / (RAND_MAX / 2); //r is random value [0..1]
+		if (r)
+		{
+			vector_of_elements.push_back(new ConcreteElementA);
+		}
+		else
+		{
+			vector_of_elements.push_back(new ConcreteElementB);
+		}
+	}
+
+	ConcreteVisitor01 visitor01;
+	ConcreteVisitor02 visitor02;
+
+	for (auto element : vector_of_elements) element->Accept(&visitor01);
+	for (auto element : vector_of_elements) element->Accept(&visitor02);
+
+	for (auto element : vector_of_elements) delete element;
+
+	auto results01 = visitor01.GetCounters();
+	std::cout << "Final visiter01 counters of element A is " << std::get<0>(results01) << std::endl;
+	std::cout << "Final visiter01 counters of element B is " << std::get<1>(results01) << std::endl;
+
+	auto results02 = visitor02.GetCounters();
+	std::cout << "Final visiter02 counters of element A is " << std::get<0>(results02) << std::endl;
+	std::cout << "Final visiter02 counters of element B is " << std::get<1>(results02) << std::endl;
+
 	std::cout << ">>> Whole Pattern was terminated...\n";
 }
--- ConsolePatterns/Visitor.h
+++ ConsolePatterns/Visitor.h
@@ -1,5 +1,93 @@
 #pragma once
 #include <iostream>
+#include <tuple>
+
+class ConcreteElementA;
+class ConcreteElementB;
+
+class IVisitor
+{
+public:
+	IVisitor() { }
+	virtual ~IVisitor() { }
+	virtual void VisitConcreteElementA(ConcreteElementA*) = 0;
+	virtual void VisitConcreteElementB(ConcreteElementB*) = 0;
+};
+
+class ConcreteVisitor01 : public IVisitor
+{
+public:
+	void VisitConcreteElementA(ConcreteElementA* elementA) override;
+	void VisitConcreteElementB(ConcreteElementB* elementB) override;
+	std::tuple<int, int> GetCounters()
+	{
+		return std::make_tuple(elementsA_counter_, elementsB_counter_);
+	}
+
+private:
+	int elementsA_counter_ = 0;
+	int elementsB_counter_ = 0;
+};
+
+class ConcreteVisitor02 : public IVisitor
+{
+public:
+	void VisitConcreteElementA(ConcreteElementA* elementA) override;
+	void VisitConcreteElementB(ConcreteElementB* elementB) override;
+	std::tuple<int, int> GetCounters()
+	{
+		return std::make_tuple(elementsA_counter_, elementsB_counter_);
+	}
+
+private:
+	int elementsA_counter_ = 0;
+	int elementsB_counter_ = 0;
+};
+
+
+class Element
+{
+public:
+	virtual ~Element() { }
+
+	virtual void Accept(IVisitor* visitor) = 0;
+
+private:
+};
+
+class ConcreteElementA : public Element
+{
+public:
+	void Accept(IVisitor* visitor) override
+	{
+		visitor->VisitConcreteElementA(this);
+	}
+
+	int OperationA()
+	{
+		return data_;
+	}
+private:
+	int data_ = 11;
+};
+
+class ConcreteElementB : public Element
+{
+public:
+	void Accept(IVisitor* visitor) override
+	{
+		visitor->VisitConcreteElementB(this);
+	}
+
+	int OperationB()
+	{
+		return data_;
+	}
+private:
+	int data_ = 22;
+};
+
+
 
 class Visitor
 {
